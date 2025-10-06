4ea60164cf050d462b9587b077284657fe1e9297
oliwia-j <olivska@gmail.com>
2025-10-06 16:42:40 +0100

    Moved code from main into game_system start.

--- game_system.cpp
+++ game_system.cpp
@@ -2,10 +2,36 @@
 #include "game_parameters.hpp"
 
 using param = Parameters;
-using gs = GameSystem;
 
 void GameSystem::start(unsigned int width, unsigned int height,
     const std::string& name, const float& time_step) {
+
+    sf::RenderWindow window(sf::VideoMode({ width, height }), name);
+    _init();
+    sf::Event event;
+    while (window.isOpen()) {
+        static sf::Clock clock;
+        float dt = clock.restart().asSeconds();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+                clean();
+                return;
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+            window.close();
+        }
+        window.clear();
+        _update(dt);
+        _render(window);
+        // Wait for the time_step to finish before displaying the next frame
+        sf::sleep(sf::seconds(time_step));
+        // Wait for Vsync
+        window.display();
+    }
+    window.close();
+    clean();
 }
 
 void GameSystem::clean() {
--- main.cpp
+++ main.cpp
@@ -1,37 +1,9 @@
-#include <SFML/Graphics.hpp>
 #include "game_parameters.hpp"
 #include "game_system.hpp"
 
+using param = Parameters;
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ Parameters::game_width, Parameters::game_height }), "GE_LAB");
-    window.setVerticalSyncEnabled(true);
-    GameSystem::init();
-
-    while (window.isOpen()) {
-        // Reset clock, recalculate deltatime
-        static sf::Clock clock;
-        float dt = clock.restart().asSeconds();
-
-        // Check and consume events
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-                return 0;
-            }
-        }
-        // Quit Via ESC Key
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-
-        window.clear();
-        GameSystem::update(dt);
-        GameSystem::render(window);
-        // Wait for the time_step to finish before displaying the next frame
-        sf::sleep(sf::seconds(Parameters::time_step));
-        // Wait for Vsync
-        window.display();
-    }
-    return 0;
+	GameSystem::start(param::game_width, param::game_height, "GE-LAB", param::time_step);
+	return 0;
 }
